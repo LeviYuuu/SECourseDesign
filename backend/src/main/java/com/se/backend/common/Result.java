cc9b98a996b90d7bc5f4e2b689a9813fcdf35bde
LeviYuuu <1057049426@qq.com>
2026-01-10 11:05:57 +0800

    1

--- backend/src/main/java/com/se/backend/common/Result.java
+++ backend/src/main/java/com/se/backend/common/Result.java
@@ -11,7 +11,7 @@ public class Result<T> {
     // 有参的成功方法
     public static <T> Result<T> success(T data) {
         Result<T> result = new Result<>();
-        result.setCode(200);
+        result.setCode(1);
         result.setMsg("success");
         result.setData(data);
         return result;
